037e78beedf52160881fcf9fc11e9223a31c0188
ldrozdiak <71947456+ldrozdiak@users.noreply.github.com>
2020-11-15 13:53:15 -0700

    Add files via upload

---
+++ Coursera Capstone.py
@@ -0,0 +1,24 @@
+
+# coding: utf-8
+
+# This notebook will be used for the Coursera Capstone project. 
+
+# In[1]:
+
+import pandas as pd
+
+
+# In[2]:
+
+import numpy as np
+
+
+# In[3]:
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
